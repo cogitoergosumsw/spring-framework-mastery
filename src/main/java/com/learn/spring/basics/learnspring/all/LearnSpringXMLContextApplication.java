43204f6880fe2c11a7bebb2033625ec07014bf63
Seng Wee <sengwee_new@hotmail.com>
2018-07-25 18:14:59 +0800

    setup for XML application context

--- src/main/java/com/learn/spring/basics/learnspring/all/LearnSpringXMLContextApplication.java
+++ src/main/java/com/learn/spring/basics/learnspring/all/LearnSpringXMLContextApplication.java
@@ -2,27 +2,31 @@ package com.learn.spring.basics.learnspring.all;
 
 import com.learn.spring.basics.learnspring.all.basic.BinarySearchImpl;
 import com.learn.spring.basics.learnspring.all.xml.XMLPersonDAO;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class LearnSpringXMLContextApplication {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(LearnSpringXMLContextApplication.class);
 	public static void main(String[] args) {
 
-        // BinarySearchImpl binarySearch =
-        // new BinarySearchImpl(new QuickSortAlgorithm());
         // Application Context
         try (ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("applicationContext.xml")) {
+
+            LOGGER.info("Beans loaded - {}", (Object) applicationContext.getBeanDefinitionNames());
+
             XMLPersonDAO xmlPersonDAO =
                     applicationContext.getBean(XMLPersonDAO.class);
 
-            System.out.println(xmlPersonDAO);
-            System.out.println(xmlPersonDAO.getXmlJdbcConnection());
+//            System.out.println(xmlPersonDAO);
+//            System.out.println(xmlPersonDAO.getXmlJdbcConnection());
+
+            LOGGER.info("xmlPersonDAO - {}, xmlJdbcConnection - {}", xmlPersonDAO, xmlPersonDAO.getXmlJdbcConnection());
         };
 	}
 }

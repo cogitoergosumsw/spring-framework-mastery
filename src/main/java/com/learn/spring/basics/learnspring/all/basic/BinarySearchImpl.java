554f754a6a68915a79ac5203b994752f3ffc2d56
Seng Wee <sengwee_new@hotmail.com>
2018-07-25 14:40:11 +0800

    Post Construct & Pre Destroy

--- src/main/java/com/learn/spring/basics/learnspring/all/basic/BinarySearchImpl.java
+++ src/main/java/com/learn/spring/basics/learnspring/all/basic/BinarySearchImpl.java
@@ -4,13 +4,19 @@ package com.learn.spring.basics.learnspring.all.basic;
  * Created by didi on 2018/7/24.
  */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class BinarySearchImpl {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     @Qualifier("quick")
     private SortAlgorithm sortAlgorithm;
@@ -22,4 +28,17 @@ public class BinarySearchImpl {
         // Search the array
         return 3;
     }
+
+
+//    !!! The name of the methods does not matter as long as u put the @PostConstruct and @PreDestroy before the methods
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Post Construct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("Pre Destroy");
+    }
+
 }

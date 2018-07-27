51e31788ff7d2e0b3177e2e848daaa21a7a92452
Seng Wee <sengwee_new@hotmail.com>
2018-07-27 10:59:40 +0800

    XML Configuration Unit Test

---
+++ src/test/java/com/learn/spring/basics/learnspring/BinarySearchXMLConfigurationTest.java
@@ -0,0 +1,31 @@
+package com.learn.spring.basics.learnspring;
+
+import com.learn.spring.basics.learnspring.all.LearnSpringApplication;
+import com.learn.spring.basics.learnspring.all.basic.BinarySearchImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by didi on 2018/7/27.
+ */
+
+//load the context
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class BinarySearchXMLConfigurationTest {
+
+    //get the bean from context
+    @Autowired
+    BinarySearchImpl binarySearch;
+
+    @Test
+    public void testBasicScenario() {
+//      call method on binarySearch
+        int result = binarySearch.binarySearch(new int[]{}, 5);
+        Assert.assertEquals(3, result);
+    }
+}
